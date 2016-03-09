5d269c2e8600aea0dcc0d575713086b7cf360340
Ouyang Yingjie <ouyang_yingjie@outlook.com>
2016-03-10 00:11:34 +0800

    feat:receipt_page tasking1 finish

---
+++ js/src/receipt.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  generateReceipt();
+  setReceipt();
+  clearCart();
+  backButtonClick();
+});
+
+function generateReceipt() {
+  var carts = getLocalStorage("carts");
+  var items = getLocalStorage("items");
+  var receiptList = getLocalStorage("receiptList");
+  var myDate = new Date();
+  var time = myDate.toGMTString();
+
+  $("#time").text("时间" + time);
+  $("#operator").text("操作员：老司机");
+
+  carts.forEach(function(cart) {
+    items.forEach(function(item) {
+      if(cart.id === item.id) {
+        var htmlContext = "";
+        htmlContext += "<tr>";
+        htmlContext += "<td>" + item.name + "</td>";
+        htmlContext += "<td>" + item.price + "</td>";
+        htmlContext += "<td>" + cart.count + "</td>";
+        htmlContext += "<td>" + item.price * cart.count + "</td>";
+        htmlContext += "</tr>";
+        $("table").append(htmlContext);
+        receiptList.push({id:cart.id, subtotal: item.price * cart.count});
+      }
+    });
+  });
+}
+
